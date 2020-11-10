112f42c5919b74d9a2933e84f4442315d6997df1
vamshi7589 <vamshi7589@gmail.com>
2020-11-10 23:02:22 +0530

    added: first commit

---
+++ EmpWageComputation.java
@@ -0,0 +1,9 @@
+public class EmployeeWageComputation
+{
+	final int isFullTime=1;
+	final int isPartTime=2;
+	final int partTimeHours=4;
+	final int fullTimeHours=8;
+	final int empRatePerHour=20;
+	final int maxHours=100;
+        final int maxDays=20;
