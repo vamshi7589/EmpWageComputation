a5538cd9510a015f109bc489061e010407dbfe6e
vamshi7589 <vamshi7589@gmail.com>
2020-11-10 23:06:26 +0530

    added: added employee attendance program

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -7,3 +7,22 @@ public class EmployeeWageComputation
 	final int empRatePerHour=20;
 	final int maxHours=100;
         final int maxDays=20;
+public int empAttendance() {
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		return empCheck;
+	}
+	public void welcomeMessage() {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+	public void checkAttendence(){
+		switch (empAttendance()) {
+		case isFullTime:
+			System.out.println("Employee is full time");
+			break;
+		case isPartTime:
+			System.out.println("Employee is part time");
+			break;
+		default:
+			System.out.println("Employee is absent");
+		}
+	}
